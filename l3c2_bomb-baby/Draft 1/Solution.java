e0528c9e56a243424fb160ff9d0ab731a77363f4
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-13 18:18:23 +0530

    Init L3C2 (Bomb Baby)

---
+++ l3c2_bomb-baby/Draft 1/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        // get the start time
+        long start = System.nanoTime();
+
+        // call the method
+
+        // System.out.println(obj.solution("1045"));
+
+        // get the end time
+        long end = System.nanoTime();
+
+        // execution time
+        long execution = end - start;
+        System.out.println("\nExecution time: " + execution + " nanoseconds");
+
+    }
+
+    public static String solution(String x, String y) {
+        // Your code here
+        return "";
+    }
+}
