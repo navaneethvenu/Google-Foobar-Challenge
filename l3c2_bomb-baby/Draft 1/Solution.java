7de96db2ada4c44b9291964bf8129c2abef4a4a3
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-14 09:12:46 +0530

    Complete Solution Logic (int)

--- l3c2_bomb-baby/Draft 1/Solution.java
+++ l3c2_bomb-baby/Draft 1/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 
         // call the method
 
-        // System.out.println(obj.solution("1045"));
+        System.out.println(obj.solution("2", "4"));
 
         // get the end time
         long end = System.nanoTime();
@@ -20,7 +20,35 @@ public class Solution {
     }
 
     public static String solution(String x, String y) {
+
+        int requiredMach = Integer.parseInt(x);
+        int requiredFac = Integer.parseInt(y);
+        int cycles = cycle(requiredMach, requiredFac);
+        System.out.println("Cycles: " + (cycles - 1));
         // Your code here
         return "";
     }
+
+    public static int cycle(int machCount, int facCount) {
+
+        int result;
+        System.out.println("machCount: " + machCount + " facCount: " + facCount);
+        if (machCount == 1 && facCount == 1) {
+            System.out.println("Ended");
+            result = 0;
+        } else if (machCount < 1 || facCount < 1) {
+            System.out.println("Impossible");
+            result = -1;
+        } else if (machCount > facCount) {
+            result = cycle(machCount - facCount, facCount);
+        } else {
+            result = cycle(machCount, facCount - machCount);
+        }
+        if (result == -1) {
+            return -1;
+        }
+        return result + 1;
+
+    }
+
 }
