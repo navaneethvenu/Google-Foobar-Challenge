1be0edab07324be3c8c186a923dadd6e7ccc482c
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-14 13:27:33 +0530

    Init L3C3 (Find the Access Codes)

---
+++ l3c3_find-the-access-codes/Draft 1/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        // get the start time
+        long start = System.nanoTime();
+
+        // call the method
+
+        // System.out.println(Solution.solution("1", "1")); // Expected output: "0"
+
+        // get the end time
+        long end = System.nanoTime();
+
+        // execution time
+        long execution = end - start;
+        System.out.println("\nExecution time: " + execution + " nanoseconds");
+
+    }
+
+    public static int solution(int[] l) {
+        return 0;
+    }
+}
