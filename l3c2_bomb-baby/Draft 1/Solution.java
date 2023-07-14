43d74b0f489a70941fbbdd49166ea7d204e1b252
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-14 13:05:01 +0530

    Pass L3 Challenge 2 (bombbaby)
    Update Logic to Simplify Removal by Dividing Instead of Subtracting wherever necessary

--- l3c2_bomb-baby/Draft 1/Solution.java
+++ l3c2_bomb-baby/Draft 1/Solution.java
@@ -10,7 +10,9 @@ public class Solution {
 
         // call the method
 
-        // System.out.println(Solution.solution("1", "1")); // Expected output: "0"
+        System.out.println(Solution.solution("1", "1")); // Expected output: "0"
+        System.out.println(Solution.solution("4", "7")); // Expected output: "0"
+        System.out.println(Solution.solution("2", "1")); // Expected output: "0"
         // System.out.println(Solution.solution("5", "5")); // Expected output: "0"
         // System.out.println(Solution.solution("10", "9")); // Expected output: "1"
         // System.out.println(Solution.solution("7", "8")); // Expected output: "1"
@@ -19,7 +21,7 @@ public class Solution {
         // System.out.println(Solution.solution("64", "16")); // Expected output: "3"
         // System.out.println(Solution.solution("32", "15")); // Expected output:
         // "impossible"
-        // System.out.println(Solution.solution("13", "128")); // Expected output: "8"
+        System.out.println(Solution.solution("13", "128")); // Expected output: "8"
         System.out.println(Solution.solution("1234567890", "9876543210")); // Expected output: "8"
 
         // get the end time
@@ -33,7 +35,7 @@ public class Solution {
 
     public static String solution(String x, String y) {
 
-        System.out.println("\n\nx: " + x + " y: " + y + "\n\n");
+        // System.out.println("\n\nx: " + x + " y: " + y + "\n\n");
 
         BigInteger requiredMach = x != "" ? new BigInteger(x) : BigInteger.ONE;
         BigInteger requiredFac = y != "" ? new BigInteger(y) : BigInteger.ONE;
@@ -45,29 +47,70 @@ public class Solution {
         }
         // Your code here
 
-        return (cycles.subtract(BigInteger.ONE).toString());
+        return (cycles.toString());
     }
 
     public static BigInteger cycle(BigInteger machCount, BigInteger facCount) {
 
         BigInteger result;
         BigInteger minusOne = BigInteger.valueOf(-1);
-        System.out.println("machCount: " + machCount + " facCount: " + facCount);
+        // System.out.println("machCount: " + machCount + " facCount: " + facCount);
         if ((machCount.compareTo(BigInteger.ONE) == 0) && (facCount.compareTo(BigInteger.ONE) == 0)) {
-            System.out.println("Ended");
+            // System.out.println("Ended");
             result = BigInteger.ZERO;
         } else if ((machCount.compareTo(BigInteger.ONE) == -1) || (facCount.compareTo(BigInteger.ONE) == -1)) {
-            System.out.println("Impossible");
+            // System.out.println("Impossible");
             result = minusOne;
         } else if (machCount.compareTo(facCount) == 1) {
-            result = cycle(machCount.subtract(facCount), facCount);
+            BigInteger modResult = machCount.mod(facCount);
+            boolean divide = modResult.compareTo(BigInteger.ONE) != -1;
+            boolean hasRemainder = machCount.divide(facCount).compareTo(BigInteger.ONE) == 1;
+
+            // System.out.println("modResult: " + modResult + " divide: " + divide + "
+            // hasRemainder: " + hasRemainder);
+
+            BigInteger newMachCount = divide ? machCount.mod(facCount)
+                    : hasRemainder
+                            ? (machCount.subtract(
+                                    (machCount.divide(facCount).subtract(BigInteger.ONE)).multiply(facCount)))
+                            : machCount.subtract(facCount);
+
+            result = cycle(newMachCount, facCount);
+
+            if (result.compareTo(minusOne) == 0) {
+                return minusOne;
+            }
+
+            result = divide ? result.add(machCount.divide(facCount))
+                    : hasRemainder ? machCount.divide(facCount).subtract(BigInteger.ONE) : result.add(BigInteger.ONE);
         } else {
-            result = cycle(machCount, facCount.subtract(machCount));
+            BigInteger modResult = facCount.mod(machCount);
+            boolean divide = modResult.compareTo(BigInteger.ONE) != -1;
+            boolean hasRemainder = facCount.divide(machCount).compareTo(BigInteger.ONE) == 1;
+
+            // System.out.println("modResult: " + modResult + " divide: " + divide + "
+            // hasRemainder: " + hasRemainder);
+
+            BigInteger newFacCount = divide ? facCount.mod(machCount)
+                    : hasRemainder
+                            ? (facCount.subtract(
+                                    (facCount.divide(machCount).subtract(BigInteger.ONE)).multiply(machCount)))
+                            : facCount.subtract(machCount);
+
+            result = cycle(machCount, newFacCount);
+
+            if (result.compareTo(minusOne) == 0) {
+                return minusOne;
+            }
+
+            result = divide ? result.add(facCount.divide(machCount))
+                    : hasRemainder ? facCount.divide(machCount).subtract(BigInteger.ONE) : result.add(BigInteger.ONE);
         }
         if (result.compareTo(minusOne) == 0) {
             return minusOne;
         }
-        return result.add(BigInteger.ONE);
+        // System.out.println("Result: " + result);
+        return result;
 
     }
 
