0f955671987dbd01bb0152940116005aec82407e
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-16 08:41:27 +0530

    Init L4C1 (Running with Bunnies)

---
+++ l4c1_running-with-bunnies/Draft 1/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public static void main(String[] args) {
+
+        // get the start time
+        long start = System.nanoTime();
+
+        // call the method
+
+        // System.out.println(Solution.solution(new int[] { 2, 4, 4, 8, 8, 8, 16 })); //
+
+        // get the end time
+        long end = System.nanoTime();
+
+        // execution time
+        long execution = end - start;
+        System.out.println("\nExecution time: " + execution + " nanoseconds");
+
+    }
+
+    public static int[] solution(int[][] times, int times_limit) {
+        // Your code here
+        int[] result = {};
+        return result;
+    }
+}
